48708c779a58fb5c818537f5b6622cce9f69aef7
Mats Neander <matsneander@gmail.com>
2020-06-10 22:56:24 +0200

    Uploaded files

---
+++ InputfieldSplashAndGrab.js
@@ -0,0 +1,299 @@
+$(document).ready(function () {
+
+    var images = [];
+    let apiKey = "TXtTckxpjGAtu33DZ712disnUcTMpMqR-AdPepgS0pU";
+    let startPage = 1,
+        totalHits,
+        totalPages,
+        perPage,
+        chosenOrientation,
+        chosenColor,
+        chosenOrder;
+    // Config settings as defined by PageListPermissions.module
+    var moduleConfig = config.InputfieldSplashAndGrab;
+
+    $('.unsplashSearch').val($("#Inputfield_title").val());
+    $numGridImg = $(".gridImages .gridImage:not('.gridImage--delete')").length;
+    $('.unsplashButton').on('click', function (e) {
+        e.preventDefault()
+        $maxFiles = $("#splashAndGrab").data('maxfiles');
+        $uploadedFiles = $("#splashAndGrab").data('uploadedfiles');
+        sizesFound = 0;
+        descsFound = 0;
+        $results = $('.unsplashResults');
+        $resultItems = $('.resultsItems');
+        $chosen = $('.unsplashChosen');
+        $results.html('');
+        field_name = $("#splashAndGrab").data('name');
+        page_id = $("#splashAndGrab").data('id');
+        $results.addClass('unsplashLoading');
+        
+
+        let query = $('#unsplashMagic').val();
+        $button = $(this);
+        getImages(query);
+        return false;
+    });
+    var sortValues = ['relevant', 'latest'];
+    var orientation = ['landscape', 'portrait', 'squarish'];
+    var colors = ['black_and_white', 'black', 'white', 'yellow', 'orange', 'red', 'purple', 'magenta', 'green', 'teal', 'blue'];
+    function getImages(searchterm, page = 1, per_page = 15, order, color, orientation) {
+        
+        $.getJSON("https://api.unsplash.com/search/photos",
+            {
+                client_id: apiKey,
+                query: searchterm,
+                page: page,
+                per_page: per_page,
+                order_by: order,
+                color: color,
+                orientation: orientation                  
+            },
+            function (data) {
+
+                totalHits = data.total;
+                totalPages = data.total_pages;
+                perPage = per_page;
+
+                $results.children().remove();
+
+                if (data.results.length == 0) {
+                   noHits();
+                } 
+                
+                if(data.total > 0){
+                    renderPager();
+                } 
+
+                $.each(data.results, function (item) {
+                    let itemNo = data.results[item];
+                    renderImage(itemNo);
+                });
+
+                $("#numSelected").html($(".unsplashChosen .selected").length);
+            });
+    }
+
+    function noHits(){
+        $sorryString = moduleConfig.i18n.noHits;
+        $results.html($sorryString);
+    }
+
+    $("body").on("change", "#unsplashOriantation",function(){
+        let query = $("#unsplashMagic").val();
+        chosenOrientation = this.value;
+        if (!chosenOrientation) chosenOrientation = undefined; 
+        getImages(query, startPage, perPage, chosenOrder, chosenColor, chosenOrientation);
+    });
+
+    $("body").on("change", "#unsplashColor", function () {
+        let query = $("#unsplashMagic").val();
+        chosenColor = this.value;
+        if (!chosenColor) chosenColor = undefined; 
+        getImages(query, startPage, perPage, chosenOrder, chosenColor, chosenOrientation);
+    });
+
+    $("body").on("change", "#unsplashOrder", function () {
+        let query = $("#unsplashMagic").val();
+        chosenOrder = this.value;
+        if (!chosenOrder) chosenOrder = undefined;
+        getImages(query, startPage, perPage, chosenOrder, chosenColor, chosenOrientation);
+    });
+
+    $("body").on("click", "#nextPager", function(event){
+        event.preventDefault();
+        let query = $("#unsplashMagic").val();
+        startPage++;
+        getImages(query, startPage, perPage, chosenOrder, chosenColor, chosenOrientation);
+    });
+
+    $("body").on("click", "#prevPager", function (event) {
+        event.preventDefault();
+        let query = $("#unsplashMagic").val();
+        startPage--;
+        getImages(query, startPage, perPage, chosenOrder, chosenColor, chosenOrientation);
+    });
+
+    $(document).on("click", "#numSelected", function(){
+        $(".unsplashChosen").toggle();
+    })
+
+    function addToLocalstorage(img) {
+        sessionStorage.setItem(img, img);
+    }
+
+    function removeLocalstorageItem(img) {
+        sessionStorage.removeItem(img)
+    }
+
+    function renderColorSelect(){
+        let options;
+        for (let index = 0; index < colors.length; index++) {
+
+            let configIndex = colors[index];
+            let selected = chosenColor == configIndex ? "selected" : "";
+            options += "<option " + selected + " value='" + configIndex + "'>" + moduleConfig.i18n[configIndex] + "</option>";
+        }
+        $colorsOut = `
+        <select class="uk-select" id="unsplashColor">
+        <option value="">${moduleConfig.i18n.colors}</option>
+        ${ options }
+         </select >
+        `;
+
+        return $colorsOut;
+    }
+
+    function renderOrientationSelect(){
+        let options = "";
+        for (let index = 0; index < orientation.length; index++) {
+
+            let configIndex = orientation[index];
+            let selected = chosenOrientation == configIndex ? "selected" : "";
+            options += "<option " + selected + " value='" + configIndex + "'>" + moduleConfig.i18n[configIndex] + "</option>";
+        }
+        let optionsOut = `
+        <select id="unsplashOriantation" class="uk-select">
+            <option value="">${moduleConfig.i18n.orientaions}</option>
+            ${options}
+        </select>`;
+
+        return optionsOut;
+    }
+
+    function renderOrderSelect(){
+        let options = "";
+        for (let index = 0; index < sortValues.length; index++) {
+
+            let configIndex = sortValues[index];
+            let selected = chosenOrder == configIndex ? "selected" : "";
+            options += "<option " + selected + " value='" + configIndex + "'>" + moduleConfig.i18n[configIndex] + "</option>";
+        }
+        let orderOut = `
+        <select id="unsplashOrder" class="uk-select">
+            ${options}
+        </select>`;
+
+        return orderOut;
+    }
+
+    function renderPager(){
+        let disablePrev = startPage < 2 ? "disabled" : "" ;
+        let disableNext = totalPages == startPage ? "disabled" : "";
+        
+        $("#numSelected").html();
+        let pager = `
+        <div class="uk-margin">
+            <button ${disablePrev} id="prevPager" class="uk-button uk-button-primary uk-button-small">
+            <span uk-icon='icon: chevron-left'></span></button>
+            <button ${disableNext} id="nextPager" class="uk-button uk-button-primary uk-button-small">
+                <span uk-icon='icon: chevron-right'></span>
+            </button>
+            <span class="uk-margin-left uk-text-small">${moduleConfig.i18n.numberOfSelects}: </span><span id="numSelected" class="uk-button uk-button-primary uk-button-small"></span><span class="uk-margin-small-left" title="${moduleConfig.i18n.title_numoffiles}">${moduleConfig.i18n.of} ${$maxFiles}</span>
+            ${renderOrientationSelect()}
+            ${renderColorSelect()}
+            ${renderOrderSelect()}
+            <span>${moduleConfig.i18n.totalhitsstring}: ${totalHits}  </span>
+            </div>`;
+        $(pager).prependTo($results);
+    }
+
+    
+    $(".gridImages").on("click", ".gridImage, .gridImage--delete", function(){
+        setTimeout(() => {
+            $numGridImg = $(".gridImages .gridImage:not('.gridImage--delete')").length;
+    
+        }, 100);
+        
+            
+        })
+    
+    function shake(thing) {
+        var interval = 100;
+        var distance = 10;
+        var times = 6;
+
+        for (var i = 0; i < (times + 1); i++) {
+            $(thing).animate({
+                left:
+                    (i % 2 == 0 ? distance : distance * -1)
+            }, interval);
+        }
+        $(thing).animate({
+            left: 0,
+            top: 0
+        }, interval);
+    }
+
+    
+    function renderImage(item) {
+        let firstName = item.user.first_name;
+        let lastName = item.user.last_name ? " " + item.user.last_name : "";
+        let photographer = firstName + lastName;
+        let selected = sessionStorage.getItem(item.id) == item.id ? "selected" : "" ; 
+        let userUrl = item.user.links.html + "?utm_source=inputfieldSmashAndGrab&utm_medium=referral "; 
+        $("<div data-uid='" + item.id + "' class='fImage gridImg " + selected + "' style='background-image: url(" + item.urls.small + ")'>"
+            + "<a target='_blank' href='" + userUrl + "'><div class='userData'><img src='" + item.user.profile_image.medium + "'><p>" + photographer + "</p></div></a></div>")
+           
+            .appendTo($results)
+            .on("click", function (e) {
+                $target = $(e.target);
+                if ($(".unsplashChosen .selected").length >= $maxFiles - $numGridImg && !$target.hasClass("selected")){
+                    console.log("waz up");
+                    shake($(this));
+                    return
+                }
+                $target = $(e.target);
+                if ( $target.hasClass("userData") || $target.parent().hasClass("userData") ){
+                    return;
+                }
+
+                if (!$target.hasClass("selected")) {
+                    renderSelected(item);
+                    $(e.currentTarget).addClass("selected");
+                    addToLocalstorage(item.id);
+                    $("#numSelected").html($(".unsplashChosen .selected").length);
+                    $(".unsplashChosen").show();
+                } else {
+                    $(".unsplashChosen .selected[data-uid='" + item.id + "']").remove();
+                    $(e.currentTarget).removeClass("selected");
+                    removeLocalstorageItem(item.id);
+                    $("#numSelected").html($(".unsplashChosen .selected").length);
+                }
+            
+            });
+       
+    }
+
+    function renderSelected(pic) {
+        let firstName = pic.user.first_name;
+        let lastName = pic.user.last_name ? " " + pic.user.last_name : "";
+        let photographer = firstName + lastName;
+        let description = pic.description ? pic.description : pic.alt_description;
+        $("<div data-download='" + pic.links.download_location + "' data-uid='" + pic.id + "' class='picked fImage selected' style='background-image: url(" + pic.urls.small + ")'>"
+            + "<input id='id-" + page_id + "-" + pic.id + "' type='hidden' name='unsplash_" + field_name + "*" + field_name + "*" + page_id + "*[]' value='" + pic.urls.regular + "*" + description + ". " + moduleConfig.i18n.photo_by + " " + photographer + ", Unsplash.' />"
+            +"<span class='uk-text-primary' uk-icon='icon: check'></span>"
+            +"</div>")
+        .appendTo($chosen)
+            .on("click", function (e) {
+                $target = $(e.target);
+                $(".gridImg[data-uid='" + pic.id + "']").first().removeClass("selected");
+                $target.remove();
+                removeLocalstorageItem(pic.id);
+            });
+    }
+
+
+
+    window.addEventListener("beforeunload", function (evnt) {
+        $(".unsplashChosen").children().each(function(){
+            $.ajax({
+                type: 'GET',
+                async: true,
+                url: $(this).data("download") + '?client_id=' + apiKey,
+            });
+            
+        })
+        sessionStorage.clear();
+    });
+}); 
---
+++ InputfieldSplashAndGrab.module.php
@@ -0,0 +1,200 @@
+<?php
+namespace ProcessWire;
+
+class InputfieldSplashAndGrab extends InputfieldImage implements ConfigurableModule {
+
+	public static function getModuleInfo() {
+		return array(
+			'title' => 'SplashAndGrab',
+			'version' => 100,
+			'icon' => 'picture-o',
+			'author' => 'Mats Neander',
+			'summary' => 'Adds possibility to search and download free to use images from Unsplash.',
+			'autoload' => 'template=admin',
+			'requires' => 'ProcessWire>=3.0.0'
+			);
+	}
+
+	public $maxFiles;
+	public $uploadedFiles; 
+
+
+	public static function getImagefields() {
+    	$imgFields = wire('fields');
+		foreach ($imgFields as $imgField) { 
+			if ($imgField->type != "FieldtypeImage") {
+				continue;
+			}
+			$imgFieldsOut[] = $imgField->name;
+		}
+		return $imgFieldsOut;
+	}
+
+	/**
+	 * Ready
+	 */
+	public function ready() {
+		//Get current pages imagefields
+		$currentPage = $this->pages->get($this->input->get->id);
+		$currentFields = $currentPage->fields;
+        foreach ($currentFields as $cf) {
+            if ($cf->type == "FieldtypeImage" and in_array($cf->name, $this->useField)){
+				$fieldName = $cf->name;
+				$this->uploadedFiles = count($currentPage->$fieldName);
+				$this->maxFiles = $cf->maxFiles;
+
+				$this->addHookAfter('InputfieldImage::render', $this, 'modifyInputfield');
+				$this->addHookBefore('ProcessPageEdit::execute', $this, 'addDependencies');
+				$this->addHookAfter('ProcessPageEdit::processInput', $this, 'processInput');
+				}
+		}	
+	}
+
+	/**
+	 * Modify inputfield
+	 *
+	 * @param HookEvent $event
+	 */
+	protected function modifyInputfield(HookEvent $event) {
+		// Only for ProcessPageEdit or ProcessUser
+		if($this->process != 'ProcessPageEdit' && $this->process != 'ProcessUser') return;
+
+		$inputfield = $event->object;
+		$out = $event->return;
+		$page = $inputfield->hasPage;
+		$field = $inputfield->hasField;
+
+		$attrs = $this->getAttributes();
+			
+		$out .= '<hr /><fieldset id="splashAndGrab" data-uploadedfiles="' . $this->uploadedFiles . '" data-maxfiles="' . $this->maxFiles . '" class="unsplash" data-id="' . $page->id . '" data-name="'. $field->name .'">';
+		$out .= '<div uk-grid class="uk-flex-bottom">';
+		$out .= '<div>';
+		$out .= '<label for="unsplashMagic" class="uk-form-label">' . __("Search Unsplash") . '';
+		$out .= '<input type="text" id="unsplashMagic" class="unsplashSearch"/></label>';
+		$out .= '</div>';
+		$out .= '<div>';
+		$out .= '<button class="unsplashButton ui-button ui-widget ui-corner-all ui-state-default"><span class="ui-button-text">' . __("Search") . '</span></button>';
+		$out .= '</div>';
+		$out .= '</div>';
+		$out .= '<div class="unsplashChosen"></div><div class="unsplashResults"><div class="resultsItems"></div></div></fieldset>';
+		$event->return = $out;
+
+	}
+
+	/**
+	 * Add JS and CSS dependencies
+	 *
+	 * @param HookEvent $event
+	 */
+	protected function addDependencies(HookEvent $event) {
+		// Return if ProcessPageEdit is being loaded via AJAX
+		if($this->config->ajax) return;
+		$this->wire('modules')->get('InputfieldImage');
+
+		// Add JS and CSS dependencies
+		$config = $this->config;
+		$info = $this->getModuleInfo();
+		$version = $info['version'];
+		$config->scripts->add($config->urls->{$this} . "{$this}.js?v={$version}");
+		$config->styles->add($config->urls->{$this} . "{$this}.css?v={$version}");
+
+		$this->config->js('InputfieldSplashAndGrab', array(
+            'i18n' => array(
+				'noHits' => __("Sorry. No hits for your query."),
+				'totalhitsstring' => __("Hits"),
+				'numberOfSelects' => __("Selected images"),
+				'orientaions' => __("All orientations"),
+				'colors' => __("All colors"),
+				'portrait' => __("Portrait"),
+				'landscape' => __("Landscape"),
+				'squarish' => __("Squarish"),
+				'latest' => __("Latest"),
+				'relevant' => __("Relevant"),
+				'black_and_white' => __("Black and White"),
+				'black' => __("Black"),
+				'white' => __("White"),
+				'yellow' => __("Yellow"),
+				'orange' => __("Orange"),
+				'red' => __("Red"),
+				'purple' => __("Purple"),
+				'magenta' => __("Magenta"),
+				'green' => __("Green"),
+				'teal' => __("Tealish"),
+				'blue' => __("Blue"),
+				'of' => __("of"),
+				'title_numoffiles' => __("The number of files you can select based on how many images you can upload and that are uploaded to this image field."),
+				'photo_by' => __("Photo by"),
+			)
+        ));
+
+	}
+
+	/**
+	 * Process URLs in $input
+	 *
+	 * @param HookEvent $event
+	 */
+	protected function processInput(HookEvent $event) {
+        $form = $event->arguments(0);
+        // Only for main Page Edit form
+        if ($form->name !== 'ProcessPageEdit') {
+            return;
+        }
+
+        foreach ($this->input->post as $key => $value) {
+
+            // Ignore unrelated input
+            if (substr($key, 0, 8) !== 'unsplash' || empty($value)) {
+                continue;
+            }
+
+            // Get variables from key name
+            list($junk, $field_name, $page_id, $morejunk) = explode('*', $key);
+            $field_name = $this->sanitizer->fieldName($field_name);
+            $page = $this->pages->get((int) $page_id);
+            $field = $this->fields->get($field_name);
+            $is_image_field = $field->type instanceof FieldtypeImage;
+			$field_value = $page->getUnformatted($field_name);
+			
+			foreach ($value as $key => $val) {
+				$img = explode("*", $val, 2); // $image[0] is url, $image[1] is description
+
+				$page->of(false);
+            if ($field->maxFiles == 1 && count($field_value)) {
+                $field_value->removeAll();
+            }
+
+            $pagefile = new Pageimage($field_value, $img[0]);
+			$field_value->add($pagefile);
+			$image = $field_value->last();
+			$image->description = $img[1];
+			$page->save($field_value);
+			}
+		}
+    }	
+
+	/**
+	 * Config inputfields
+	 *
+	 */
+	public function getModuleConfigInputfields( array $data) {
+		$modules = $this->wire('modules');
+		$inputfields = new InputfieldWrapper();
+
+        $fieldImage = $modules->get("InputfieldAsmSelect");
+        $fieldImage->name = "useField";
+        $fieldImage->label = __("Image fields that should use the module.");
+        $fieldImage->description = __("Choose the fields which should use the module.");
+        foreach ($this->getImagefields() as $img) {
+            $fieldImage->addOption($img);
+		}
+        $fieldImage->value = $data['useField'];
+        //$fieldTemplate->notes = "Number of pages " . count($reminders) . ".";
+		
+		$inputfields->add($fieldImage);
+
+		return $inputfields;
+
+
+	}
+}
